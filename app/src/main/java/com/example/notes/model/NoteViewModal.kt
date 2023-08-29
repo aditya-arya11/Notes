9103eb75b88ac7cd57ddd4d88c35373d0cbb6dd6
aditya-arya11 <adityaarya395@gmail.com>
2023-08-29 09:27:25 +0530

    Initial commit

--- app/src/main/java/com/example/notes/model/NoteViewModal.kt
+++ app/src/main/java/com/example/notes/model/NoteViewModal.kt
@@ -1,72 +1,39 @@
 package com.example.notes.model
 
 import com.example.notes.database.Note
-import com.example.notes.ActivityAddEditNoteBinding
-import com.example.notes.model.NoteViewModal
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import java.text.SimpleDateFormat
-import java.util.*
+import com.example.notes.database.NoteDatabase
+import com.example.notes.database.NoteRepository
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AddEditNoteActivity : AppCompatActivity() {
+class NoteViewModal(application: Application): AndroidViewModel(application) {
 
-    private lateinit var binding: ActivityAddEditNoteBinding
-    lateinit var viewModal: NoteViewModal
-    var noteID = -1;
 
-    @SuppressLint("SimpleDateFormat")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityAddEditNoteBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    val allNotes: LiveData<List<Note>>
+    val repository: NoteRepository
 
-        viewModal = ViewModelProvider(
-            this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
-        )[NoteViewModal::class.java]
-
-        val noteType = intent.getStringExtra("noteType")
-
-        if (noteType.equals("Edit")) {
-            val noteTitle = intent.getStringExtra("noteTitle")
-            val noteDescription = intent.getStringExtra("noteDescription")
-            noteID = intent.getIntExtra("noteId", -1)
-            binding.idBtn.text = "Update Note"
-            binding.idEdtNoteName.setText(noteTitle)
-            binding.idEdtNoteDesc.setText(noteDescription)
-        } else {
-            binding.idBtn.text = "Save Note"
-        }
-
-        binding.idBtn.setOnClickListener {
-            val noteTitle = binding.idEdtNoteName.text.toString()
-            val noteDescription = binding.idEdtNoteDesc.text.toString()
-
-            if (noteType.equals("Edit")) {
-                if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
-                    val sdf = SimpleDateFormat("dd MMM, yyyy - HH:mm")
-                    val currentDateAndTime: String = sdf.format(Date())
-                    val updatedNote = Note(noteTitle, noteDescription, currentDateAndTime)
-                    updatedNote.id = noteID
-                    viewModal.updNote(updatedNote)
-                    Toast.makeText(this, "Note Updated..", Toast.LENGTH_LONG).show()
-                }
-            } else {
-                if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
-                    val sdf = SimpleDateFormat("dd MMM, yyyy - HH:mm")
-                    val currentDateAndTime: String = sdf.format(Date())
-                    viewModal.insertNote(Note(noteTitle, noteDescription, currentDateAndTime))
-                    Toast.makeText(this, "$noteTitle Added", Toast.LENGTH_LONG).show()
-                }
-            }
-            // opening the new activity on below line
-            //startActivity(Intent(applicationContext, MainActivity::class.java))
-            this.finish()
-        }
-
+  
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNotesDao()
+        repository = NoteRepository(dao)
+        allNotes = repository.allNotes
     }
+
+    fun delNote(note: Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.delete(note)
+    }
+
+    fun updNote(note: Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.update(note)
+    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.insert(note)
+    }
+
+
 }
